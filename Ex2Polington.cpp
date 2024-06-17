b8bf2a69e6c4ba6cddd7c6206ae984b6d5bf4da3
Ilya Avramchuk <163721512+randredNSTU@users.noreply.github.com>
2024-06-18 04:04:47 +0700

    Add files via upload

--- Ex2Polington.cpp
+++ Ex2Polington.cpp
@@ -8,208 +8,176 @@
 
 using namespace std;
 
-// Функция для нахождения всех простых чисел до n (решето Эратосфена)
+// Функция для получения таблицы простых чисел
 vector<int> primes(int n);
-
-// Функция для построения числа и проведения теста
 pair<int, vector<int>> builder_test(vector<int> prime, int bit);
-
-// Тест Поклингтона для проверки числа на простоту
 int test_poklin(int n, int t, vector<int> q);
 
-// Функция для возведения числа a в степень b по модулю n
 int power_mod(int a, int b, int n);
-
-// Функция для генерации случайного числа в диапазоне [a, b]
 int rn(int a, int b);
 
-// Функция для вывода результатов
 void print_results(const vector<int>& res, const vector<string>& res_ver_test, const vector<int>& otvegnutie);
 
 int main() {
-    int size_primes = 500;  // Размер для генерации простых чисел
-    vector<int> prime = primes(size_primes);  // Генерация простых чисел
+    int size_primes = 500; // Размер таблицы малых простых чисел
+    vector<int> prime = primes(size_primes); // Строим таблицу малых простых чисел
 
-    int bit = 0;  // Размер числа в битах
-    cin >> bit;  // Ввод размера числа
+    int bit = 0;
+    cin >> bit; // Запрашиваем размер требуемого простого числа
 
-    vector<int> q;  // Вектор для хранения множителей
-    int n;  // Сгенерированное число
-    int k = 0;  // Счетчик отвергнутых чисел
-    int probability;  // Вероятность простоты
+    vector<int> q; // Вектор для хранения факторов F
+    int n; // Переменная для хранения числа n
+    int k = 0; // Счетчик для отклоненных чисел
+    int probability; // Переменная для хранения результата теста
 
-    vector<int> res;  // Вектор для хранения результатов
-    vector<string> res_ver_test;  // Вектор для хранения результатов теста
-    vector<int> otvegnutie;  // Вектор для хранения количества отвергнутых чисел
+    vector<int> res; // Вектор для хранения найденных простых чисел
+    vector<string> res_ver_test; // Вектор для хранения результатов дополнительного теста
+    vector<int> otvegnutie; // Вектор для хранения количества отклоненных чисел
 
-    // Генерация 10 различных простых чисел
-    while (res.size() != 10) {
-        // Построение числа и проведение теста
-        tie(n, q) = builder_test(prime, bit);
-        probability = test_poklin(n, 10, q);
+    while(res.size() != 10) { // Повторяем, пока не найдем 10 простых чисел
+        tie(n, q) = builder_test(prime, bit); // Строим число n и получаем разложение F
+        probability = test_poklin(n, 10, q); // Проверяем число n с параметром надежности 10
 
-        // Проверка на простоту
-        if (probability == 1) {
-            // Проверка на уникальность числа
-            if (find(res.begin(), res.end(), n) == res.end()) {
-                res.push_back(n);  // Добавление числа в результаты
+        if(probability == 1) { // Если n прошло тест Поклингтона
+            if (find(res.begin(), res.end(), n) == res.end()) { // Если n еще не добавлено в результат
+                res.push_back(n); // Добавляем n в результат
 
-                // Дополнительная проверка числа
-                probability = test_poklin(n, 1, q);
-                if (probability == 1) {
-                    res_ver_test.push_back("+");  // Число прошло проверку
+                probability = test_poklin(n, 1, q); // Проверяем число n с параметром надежности 1
+                if(probability == 1) {
+                    res_ver_test.push_back("+"); // Если n прошло тест, добавляем "+" в результат
                 } else {
-                    res_ver_test.push_back("-");  // Число не прошло проверку
+                    res_ver_test.push_back("-"); // Если n не прошло тест, добавляем "-" в результат
                 }
 
-                otvegnutie.push_back(k);  // Добавление количества отвергнутых чисел
-                k = 0;  // Сброс счетчика отвергнутых чисел
+                otvegnutie.push_back(k); // Добавляем количество отклоненных чисел
+                k = 0; // Сбрасываем счетчик отклоненных чисел
             }
         } else {
-            k++;  // Увеличение счетчика отвергнутых чисел
+            k++; // Увеличиваем счетчик отклоненных чисел
         }
     }
 
-    // Вывод результатов
-    print_results(res, res_ver_test, otvegnutie);
+    print_results(res, res_ver_test, otvegnutie); // Печатаем результаты
 }
 
-// Функция решета Эратосфена для нахождения всех простых чисел до n
 vector<int> primes(int n) {
-    vector<bool> is_prime(n + 1, true);  // Вектор для отметки простоты чисел
-    vector<int> primes;  // Вектор для хранения простых чисел
+    vector<bool> is_prime(n + 1, true); // Инициализируем вектор для проверки простых чисел
+    vector<int> primes; // Вектор для хранения простых чисел
 
-    // Реализация решета Эратосфена
-    for (int p = 2; p * p <= n; ++p) {  // Для всех чисел от 2 до sqrt(n)
-        if (is_prime[p]) {  // Если число p простое
-            for (int i = p * p; i <= n; i += p)  // Удаление кратных чисел p
-                is_prime[i] = false;
+    for (int p = 2; p * p <= n; ++p) {
+        if (is_prime[p]) {
+            for (int i = p * p; i <= n; i += p)
+                is_prime[i] = false; // Помечаем все кратные p как составные
         }
     }
 
-    // Сбор всех простых чисел в вектор
     for (int p = 2; p <= n; ++p) {
         if (is_prime[p])
-            primes.push_back(p);
+            primes.push_back(p); // Добавляем простые числа в вектор
     }
 
-    return primes;  // Возвращение вектора простых чисел
+    return primes; // Возвращаем таблицу простых чисел
 }
 
-// Функция для построения числа и проведения теста
 pair<int, vector<int>> builder_test(vector<int> prime, int bit) {
-    int max_index = 0;  // Максимальный индекс в векторе простых чисел
-    int max_pow = 1;  // Максимальная степень двойки
-
-    // Нахождение максимального индекса, такого что prime[max_index] < 2^((bit / 2) + 1)
+    int max_index = 0;
+    int max_pow = 1;
+    // Находим максимальный индекс и степень для построения числа F
     for (; (prime[max_index] < pow(2, (bit / 2) + 1)) && (max_index < prime.size()); max_index++);
-    // Нахождение максимальной степени двойки, такой что 2^max_pow < 2^((bit / 2) + 1)
     for (; pow(2, max_pow) < pow(2, (bit / 2) + 1); max_pow++);
 
-    int f = 1;  // Начальное значение f
-    vector<int> q;  // Вектор для хранения множителей
+    int f = 1; // Начальное значение F
+    vector<int> q; // Вектор для хранения факторов F
 
-    // Построение числа f
-    while (true) {
-        int num = rn(0, max_index);  // Случайный индекс простого числа
-        int power = rn(1, max_pow);  // Случайная степень
-
-        // Проверка на переполнение и добавление множителя
+    while(true){
+        int num = rn(0, max_index); // Случайный индекс для выбора простого числа
+        int power = rn(1, max_pow); // Случайная степень для выбора простого числа
+        
         if (pow(prime[num], power)) {
-            if (f * pow(prime[num], power) < INT32_MAX) {
-                f *= pow(prime[num], power);
-                q.push_back(prime[num]);
+            if(f * pow(prime[num], power) < INT32_MAX) {
+                f *= pow(prime[num], power); // Умножаем F на простое число в степени power
+                q.push_back(prime[num]); // Добавляем простое число в вектор q
             }
         }
 
-        // Проверка на соответствие условиям
-        if (f > pow(2, (bit / 2))) {
-            if (f >= pow(2, (bit / 2) + 1)) {
-                f = 1;
-                q.clear();
+        if(f > pow(2, (bit / 2))){
+            if(f >= pow(2, (bit / 2) + 1)){
+                f = 1; // Если F слишком велико, сбрасываем его
+                q.clear(); // Очищаем вектор q
             } else {
-                break;
+                break; // Если F подходит по размеру, выходим из цикла
             }
         }
     }
 
-    int R;  // Случайное четное число R
+    int R; // Переменная для хранения значения R
 
-    // Генерация случайного четного числа R
     do {
-        R = rn(pow(2, (bit / 2) - 1) + 1, pow(2, bit / 2));
+        R = rn(pow(2, (bit / 2) - 1) + 1, pow(2, bit / 2)); // Генерируем случайное четное число R
     } while (R % 2 != 0);
 
-    int n = R * f + 1;  // Инициализация числа n
-    return make_pair(n, q);  // Возвращение числа и множителей
+    int n = R * f + 1; // Вычисляем значение n
+    return make_pair(n, q); // Возвращаем значение n и вектор q
 }
 
-// Тест Поклингтона для проверки числа на простоту
-int test_poklin(int n, int t, vector<int> q) {
-    vector<int> a;  // Вектор для хранения случайных чисел
-    int aj;  // Случайное число
 
-    // Генерация t случайных чисел
+int test_poklin(int n, int t, vector<int> q) {
+    vector<int> a; // Вектор для хранения случайных чисел a
+    int aj;
+
     while (a.size() != t) {
-        aj = rn(2, n - 1);
+        aj = rn(2, n - 1); // Генерируем случайное число a
 
         if (find(a.begin(), a.end(), aj) == a.end()) {
-            a.push_back(aj);
+            a.push_back(aj); // Добавляем a в вектор, если оно еще не добавлено
         }
     }
 
-    // Первоначальная проверка простоты
     for (int aj : a) {
         if (power_mod(aj, n - 1, n) != 1) {
-            return 0;
+            return 0; // Если a^(n-1) mod n != 1, возвращаем 0 (n составное)
             break;
         }
     }
 
-    // Дополнительная проверка простоты
     bool flag = true;
     int i = 0;
     for (int aj : a) {
         if (q[i] != 0 && power_mod(aj, (n - 1) / q[i], n) == 1) {
             flag = false;
-            return 0;
+            return 0; // Если a^((n-1)/q) mod n == 1, возвращаем 0 (n составное)
             break;
         }
     }
 
-    // Проверка результата
     if (flag) {
-        return 1;
+        return 1; // Если все проверки пройдены, возвращаем 1 (n вероятно простое)
     }
 
     return 1;
 }
 
-// Функция для возведения числа a в степень b по модулю n
 int power_mod(int a, int b, int n) {
-    long long result = 1;  // Начальное значение результата
-    while (b > 0) {  // Пока степень больше 0
-        if (b % 2 == 1)  // Если степень нечетная
-            result = (result * a) % n;  // Умножение результата на a по модулю n
-        a = (a * a) % n;  // Возведение a в квадрат по модулю n
-        b /= 2;  // Деление степени на 2
+    long long result = 1;
+    while (b > 0) {
+        if (b % 2 == 1)
+            result = (result * a) % n; // Если b нечетное, умножаем результат на a
+        a = (a * a) % n; // Квадрат a по модулю n
+        b /= 2; // Делим b на 2
     }
-    return result;  // Возвращение результата
+    return result; // Возвращаем результат
 }
 
-// Функция для генерации случайного числа в диапазоне [a, b]
 int rn(int a, int b) {
-    mt19937 mt_rand(random_device{}());  // Инициализация генератора случайных чисел
-    return uniform_int_distribution<int>(a, b)(mt_rand);  // Возвращение случайного числа
+    mt19937 mt_rand(random_device{}()); // Инициализируем генератор случайных чисел
+    return uniform_int_distribution<int>(a, b)(mt_rand); // Возвращаем случайное число в диапазоне [a, b]
 }
 
-// Функция для вывода результатов
 void print_results(const vector<int>& res, const vector<string>& res_ver_test, const vector<int>& otvegnutie) {
-    cout << "Prime Numbers\tTest Results\tOccurrences" << endl;  // Заголовок таблицы
-    cout << "----------------------------------------------" << endl;  // Разделитель
+    cout << "Prime Numbers\tTest Results\tOccurrences" << endl;
+    cout << "----------------------------------------------" << endl;
 
-    // Вывод каждого результата
     for (size_t i = 0; i < res.size(); ++i) {
-        cout << res[i] << "\t\t" << res_ver_test[i] << "\t\t" << otvegnutie[i] << endl;
+        cout << res[i] << "\t\t" << res_ver_test[i] << "\t\t" << otvegnutie[i] << endl; // Печатаем результаты
     }
 }
